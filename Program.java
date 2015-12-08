912851f3b97cbc18eb4b0bd9494d4e3af353718b
dardona <danny_ardona@hotmail.com>
2015-12-07 21:01:45 -0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -5,6 +5,11 @@
  */
 package scheduletest;
 
+import com.database.config.DatabaseConnection;
+import com.database.config.Result;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Danny Ardona
@@ -14,7 +19,11 @@ public class Program {
     String programCode;
     String programName;
     
-    public Program(StudentGroupMap students, String programCode, String programName){
+    public Program(){
+        
+    }
+    
+    public Program(String programCode, String programName){
         this.students = students;
         this.programCode = programCode;
         this.programName = programName;
@@ -36,5 +45,30 @@ public class Program {
     
     public String getProgramName(){
         return programName;
+    }
+    
+    public void addProgram(){
+        DatabaseConnection connection = DatabaseConnection.open();
+        connection.addProgram(programCode, programName);
+    }
+    
+    public void removeProgram(){
+        DatabaseConnection connection = DatabaseConnection.open();
+        connection.removeProgram(programCode);
+    }
+    
+    public UserMap<Program> getMap() throws SQLException{
+        UserMap<Program> programs = new UserMap<Program>();
+        DatabaseConnection connection = DatabaseConnection.open();
+        Result result = connection.getPrograms();
+        
+        while(result.hasNext()){
+            ResultSet set = result.next();
+            Program program = new Program(set.getString("program_code"), set.getString("program_name"));
+            System.out.println(set.getString("program_code")  + ", " + set.getString("program_name"));
+            //programs.put(program);
+        }
+        
+        return programs;
     }
 }
